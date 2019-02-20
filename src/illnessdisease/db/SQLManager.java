8a8083a971035383d150f0cce0b648174c9dd8f3
Gonzalo <g.alvarez30@usp.ceu.es>
2019-02-20 17:40:44 +0100

    Connection method done

--- src/illnessdisease/db/SQLManager.java
+++ src/illnessdisease/db/SQLManager.java
@@ -1,5 +1,27 @@
 package illnessdisease.db;
+import java.sql.*;
+import java.io.*;
 
 public class SQLManager {
+	private Connection connection;
+	
+
+public void connect(String path, String classname) {
+	try {
+
+	Class.forName(classname);
+	this.connection= DriverManager.getConnection(path);
+	connection.createStatement().execute("PRAGMA foreign_keys= ON");
+	//System.out.println("database connection opened");
 
 }
+	catch(Exception e) {
+		e.printStackTrace();
+		//System.out.println("La conexion no pudo establecerse con éxito");
+	}}
+
+
+}
+
+
+
