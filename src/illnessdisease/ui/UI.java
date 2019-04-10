54f2c67f23ca39033aae7f6987c3a91a142f51ba
YorchMan <j.garcia226@usp.ceu.es>
2019-04-10 16:48:30 +0200

    .

--- src/illnessdisease/ui/UI.java
+++ src/illnessdisease/ui/UI.java
@@ -1,5 +1,49 @@
 package illnessdisease.ui;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class UI {
-
+import illnessdisease.db.*;
+//YO creo los objetos y llamo al manager para que inserte movidas
+ class UI {
+	public static void main(String args[]){
+		SQLManager manager;
+		int option;
+		manager = new SQLManager();
+		//Load tables
+		manager.createTables();
+		try{
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Select an option: ");
+            option = Integer.parseInt(consola.readLine());
+            while (true){
+            	printMenu();
+            	switch(option){
+            	case 1:
+            		System.out.println("Opción 1: Insert");
+            		manager.createTables();
+            		
+            	}
+            }
+            
+            
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public static int printMenu(){
+		int option;
+		try{
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Option 1: Insert an illness\n"
+            		+ "Option 2: Delete an illness\n");
+            System.out.println("Select an option: ");
+            option = Integer.parseInt(consola.readLine());
+            return option;
+		}catch(IOException ex){
+			ex.printStackTrace();
+		return 0;
+		}
+	}
 }
