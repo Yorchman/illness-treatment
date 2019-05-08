56e7e7a814f796773a2e481ea19181306f65afc2
YorchMan <j.garcia226@usp.ceu.es>
2019-05-08 15:52:13 +0200

    VERSION ANTIGUA :(

--- src/illnessdisease/ui/UI.java
+++ src/illnessdisease/ui/UI.java
@@ -12,24 +12,21 @@ import illnessdisease.db.*;
 		manager = new SQLManager();
 		//Load tables
 		manager.createTables();
-		try{
-            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Select an option: ");
-            option = Integer.parseInt(consola.readLine());
-            while (true){
-            	printMenu();
-            	switch(option){
-            	case 1:
-            		System.out.println("Opción 1: Insert");
-            		manager.createTables();
-            		
-            	}
-            }
-            
-            
-		}catch(IOException ex){
-			ex.printStackTrace();
+		//First we look if the user is a doctor or a patient.
+		int userType = decideUser(); //1 is patient and 2 is doctor
+		if(userType == 1){
+			while (true){
+				printMenu();
+				switch(printMenu()){
+				case 1:
+					System.out.println("Opción 1: Insert");
+					manager.createTables();            		
+				}
+			}
+		}else if(userType == 2){
+			
 		}
+        
 	}
 	
 	public static int printMenu(){
@@ -38,6 +35,20 @@ import illnessdisease.db.*;
             BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Option 1: Insert an illness\n"
             		+ "Option 2: Delete an illness\n");
+            System.out.println("Select an option: ");
+            option = Integer.parseInt(consola.readLine());
+            return option;
+		}catch(IOException ex){
+			ex.printStackTrace();
+		return 0;
+		}
+	}
+	
+	public static int decideUser(){
+		int option;
+		try{
+            BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("What kind of user are you?\n1.Patient\n2.Doctor");
             System.out.println("Select an option: ");
             option = Integer.parseInt(consola.readLine());
             return option;
